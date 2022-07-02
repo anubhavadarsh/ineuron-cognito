f6941160f3460627b891acf39d57faa8aeffad15
prabhatjoshi321 <57354589+prabhatjoshi321@users.noreply.github.com>
2022-07-02 16:32:42 +0530

    ipfs repair

--- ipfs-engine/app.js
+++ ipfs-engine/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const { create } = require("ipfs");
+const expFileUpload = require("express-fileupload");
 const fs = require("fs")
 const multer = require("multer")
 const upload = multer({ dest: "uploads/" });
@@ -8,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({  limit: "50mb", extended: false }));
 app.use(bodyParser.json());
+app.use(expFileUpload());
 
 async function ipfsClient() {
     const ipfs = await create(
@@ -32,24 +34,48 @@ app.post('/store_data', async function (req, res) {
     }
 });
 
-app.post('/store_file', upload.single("file"), async function (req, res) {
-    try {
-        let ipfs = await ipfsClient();
-        let data = fs.readFileSync("req.file")
-        let options = {
-            warpWithDirectory: false,
-            progress: (prog) => console.log(`Saved :${prog}`)
-        }
-        let result = await ipfs.add(data, options);
-        console.log(result)
-        ipfs.stop().catch(err => console.error(err))
-        return res.send(result);
+app.post("/store_file", (req, res) => {
+    let fileObj = {};
+    if (req.files.inputFile) {
+        const file = req.files.inputFile;
+        const fileName = file.name;
+        const filePath = __dirname + "/uploads/" + fileName;
 
-    } catch (e) {
-        res.end(e.message || e.toString());
+        file.mv(filePath, async (err) => {
+            if (err) {
+                console.log("Error: failed to download file.");
+                return res.status(500).send(err);
+            }
+
+            const fileHash = await addFile(fileName, filePath);
+            console.log("File Hash received __>", fileHash);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.log("Error: Unable to delete file. ", err);
+                }
+            });
+            fileObj = {
+                file: file,
+                name: fileName,
+                path: filePath,
+                hash: fileHash
+            };
+            return res.send(fileObj);
+        });
     }
 });
 
+const addFile = async (fileName, filePath) => {
+    let ipfs = await ipfsClient();
+    const file = fs.readFileSync(filePath);
+    const filesAdded = await ipfs.add({ path: fileName, content: file }, {
+    progress: (len) => console.log("Uploading file..." + len)
+    });
+    ipfs.stop().catch(err => console.error(err))
+    console.log(filesAdded);
+    const fileHash = filesAdded.cid.toString();
+    return fileHash;
+};
 
 app.post('/get', async function (req, res) {
     try {
